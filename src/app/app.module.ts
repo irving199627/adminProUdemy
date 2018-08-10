904bf4fec5ac715480fe6ea9143cb0d1392d43a4
irving199627 <irving_rosado01@hotmail.com>
2018-08-09 19:38:02 -0500

    fin de la seccion 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,28 +6,30 @@ import { APP_ROUTES } from './app.routes';
 
 // Modulos
 import { PagesModule } from './pages/pages.module';
+import { FormsModule } from '@angular/forms';
+
+// Servicios
+import { ServiceModule } from './services/service.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-// import { IncrementadorComponent } from './components/incrementador/incrementador.component';
 
-import { FormsModule } from '@angular/forms';
-// import { GraficoDonaComponent } from './components/grafico-dona/grafico-dona.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    // GraficoDonaComponent,
-    // IncrementadorComponent
   ],
   imports: [
   BrowserModule,
     APP_ROUTES,
     PagesModule,
-    FormsModule
+    FormsModule,
+    ServiceModule
   ],
   providers: [],
   bootstrap: [AppComponent]
